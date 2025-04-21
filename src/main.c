8c5febdee6940a8770d8262223e254084a692186
brycegallo <76966016+brycegallo@users.noreply.github.com>
2025-04-21 08:59:39 -0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -114,9 +114,9 @@ void process_request_buffer(struct buffer_struct *request_buffer_struct, char re
     // regcomp - expression is the string to match
     // flag - 
     int regex_comp_result = regcomp(&regex, "^GET", 0);
-    if (regex_comp_result == 0) { printf("LOG____PRB()____regex match"); }
     regfree(&regex);
-
+    int regexc_result = regexec(&regex, request_buffer);
+    if (regexc_result == 0) { printf("LOG____PRB()____regex match\n"); }
 
     printf("LOG____PRB()____Request Buffer: %s\n", request_buffer);
     //char* request_buffer_pointer = request_buffer;
